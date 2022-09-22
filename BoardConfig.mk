092ebb82ac18ee13a718dee56740fd8f46fc09ab
HELLBOY017 <abhaygill017@gmail.com>
2022-09-22 20:00:34 +0530

    instantnoodlep: Move bdroid_buildcfg to common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,9 +22,6 @@ DEVICE_PATH := device/oneplus/instantnoodlep
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OnePlus8Pro,instantnoodlep
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-
 # Display
 TARGET_SCREEN_DENSITY := 600
 
